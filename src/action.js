e48d4a113d98a07e30163348b35a94666021c659
luizinhovi <beat.luiz23@hotmail.com>
2021-11-28 17:21:58 -0300

    att

--- src/action.js
+++ src/action.js
@@ -1,10 +1,15 @@
 $(function() {
-
-
-
     setTimeout(function() {
 
         $(".intro").css("display", "none");
         $(".cardapio").css("display", "block");
     }, 2900);
 });
+
+
+function gelados() {
+    $(".gelados").css("display", "grid");
+    $("body").css("background", "red");
+    $(".gelados").css("color", "white");
+    $(".categoria").css("display", "none");
+}
