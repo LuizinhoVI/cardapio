d05f76affed454be110e4eefdb4e283348b060d3
luizinhovi <beat.luiz23@hotmail.com>
2021-11-29 13:01:31 -0300

    att

--- src/action.js
+++ src/action.js
@@ -1,10 +1,30 @@
-$(function() {
-    setTimeout(function() {
+// $(function() {
+//     setTimeout(function() {
 
-        $(".intro").css("display", "none");
-        $(".cardapio").css("display", "block");
-    }, 2900);
-});
+//         $(".intro").css("display", "none");
+//         $(".cardapio").css("display", "block");
+//     }, 2900);
+// });
+
+
+// function gelados() {
+//     $(".gelados").css("display", "grid");
+//     $("body").css("background", "red");
+//     $(".gelados").css("color", "white");
+//     $(".categoria").css("display", "none");
+// }
+
+// function refri() {
+//     $(".refri").css("display", "grid");
+//     // $("body").css("background", "red");
+//     $(".refri").css("color", "black");
+//     $(".categoria").css("display", "none");
+// }
+
+
+
+
+
 
 
 function gelados() {
@@ -16,7 +36,21 @@ function gelados() {
 
 function refri() {
     $(".refri").css("display", "grid");
-    $("body").css("background", "red");
-    $(".refri").css("color", "white");
+    // $("body").css("background", "red");
+    $(".refri").css("color", "black");
     $(".categoria").css("display", "none");
+}
+numero = 1;
+
+function foto_click() {
+
+    if (numero > 0) {
+        $(".foto").css("box-shadow", "0px 5px 5px 0px blue")[2]
+        numero = 0
+    } else {
+        $(".foto").css("box-shadow", "0px 5px 5px 0px black")[2]
+        numero = 1;
+    }
+
+
 }
